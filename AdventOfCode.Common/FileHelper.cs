a3c9acdfb5a1420f1166f5f62655ca819f6e6794
Emily Peregrine <emily.peregrine1@nhs.net>
2022-12-08 14:17:53 +0000

    Completed Day One

---
+++ AdventOfCode.Common/FileHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode.Common;
+
+public static class FileHelper
+{
+    public static string GetPathOfDataFile(string fileName)
+    {
+        return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, 
+            Path.Combine("Data", fileName));
+    }
+}
---
+++ AdventOfCode.DayOne/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//68467
+
+using System.Reflection;
+using AdventOfCode.Common;
+
+var path = FileHelper.GetPathOfDataFile("elves.txt");
+
+Console.WriteLine($"Loading the elf file from: {path}");
+
+var lines = File.ReadAllLines(path);  
+
+var elfTotals = new List<int>();
+var currentTotal = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elfTotals.Add(currentTotal);
+        currentTotal = 0;
+    }
+    else
+    {
+        var canAdd = int.TryParse(line, out var elfValue);
+        if (canAdd)
+        {
+            currentTotal += elfValue;
+        }
+    }
+}
+
+var max = elfTotals.Max();
+
+Console.WriteLine($"The largest amount is {max}");
